5b54b5130f5920c9cc891e4540728b736378ab9f
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-02-26 23:08:22 -0600

    interactive scene start
    uploaded images and basic boo movement

--- new thang/sketch.js
+++ new thang/sketch.js
@@ -3,13 +3,57 @@
 // Feb 2, 2018 - replace with the date
 
 // global variables
+let booRight, booLeft
+let speed
+let x, y
+let rightOrLeft
+
+function preload() {
+  booRight = loadImage("images/booRight.png")
+  booLeft = loadImage("images/booLeft.png")
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width / 2
+  y = height / 2
+  speed = 5
+  rightOrLeft = 0
 }
 
 function draw() {
-  background(255);
-  rect(150, 140, 150, 55, 20);
-  ellipse(180,200,45);
-  ellipse(270,200,45);
+  background(0);
+  keyPressed();
+  if (x < -width) {
+    x = width
+  }
+}
+
+function keyPressed() {
+  if (key === "w" || key === "W") {
+    y -= speed;
+    if (rightOrLeft === 0) {
+      image(booLeft, x, y);
+    } else {
+      image(booRight, x, y);
+    }
+  }
+  if (key === "s" || key === "S") {
+    y += speed;
+    if (rightOrLeft === 1) {
+      image(booRight, x, y);
+    } else {
+      image(booLeft, x, y);
+    }
+  }
+  if (key === "a" || key === "A") {
+    x -= speed;
+    image(booLeft, x, y);
+    rightOrLeft = 0
+  }
+  if (key === "d" || key === "D") {
+    x += speed;
+    image(booRight, x, y);
+    rightOrLeft = 1
+  }
 }
