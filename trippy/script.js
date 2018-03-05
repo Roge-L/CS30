1d8c95eafeeb931097dc4a8bd819a3012ee9794b
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-03-05 14:57:43 -0600

    trippy dot connection

--- trippy/script.js
+++ trippy/script.js
@@ -3,13 +3,21 @@ let distance;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  distance = 100;
+  distance = 50;
 }
 
 function draw() {
-  for (let i = 0; i < 15; i++) {
-    for (let j = 0; j < 8; i++) {
-      point(distance*i,distance*j);
+  background(0);
+  displayDots();
+}
+
+function displayDots() {
+  for (let x = distance; x < width; x += distance) {
+    for (let y = distance; y < height; y += distance) {
+      fill(255);
+      ellipse(x, y, 5, 5);
+      line(x, y, windowWidth / 2, windowHeight / 2);
+      stroke(random(0, 255), random(0, 255));
     }
   }
 }
