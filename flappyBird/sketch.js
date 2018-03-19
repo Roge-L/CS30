1b46bdb24aaa32912f463f6033ad694fe1f6bb01
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-03-18 23:43:43 -0600

    added states

--- flappyBird/sketch.js
+++ flappyBird/sketch.js
@@ -3,7 +3,6 @@
 // March 6, 2018
 
 // global variables
-
 let canvas;
 let canvasWidth, canvasHeight;
 let gravity, birdVelocity, birdAcceleration;
@@ -13,9 +12,10 @@ let map;
 let greenBar1, greenBar2, xBar1, xBar2, yBar;
 let topPipe, bottomPipe;
 let xPipe1, xPipe2, yTopPipe1, yTopPipe2, spaceBetween;
+let state;
+let title, playButton, gameOver;
 
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
+// preloading images and sounds
 function preload() {
   bird = loadImage("images/bird.png");
   map = loadImage("images/background.jpg");
@@ -23,14 +23,18 @@ function preload() {
   greenBar2 = loadImage("images/greenBar2.PNG");
   topPipe = loadImage("images/topPipe.png");
   bottomPipe = loadImage("images/bottomPipe.png");
+  title = loadImage("images/title.png");
+  playButton = loadImage("images/playButton.png");
+  gameOver = loadImage("images/gameOver.png");
 }
 
+// assigning values to variables
 function setup() {
   canvasWidth = 500;
   canvasHeight = windowHeight;
   canvas = createCanvas(canvasWidth, canvasHeight);
-  birdX = 150;
-  birdY = 0;
+  birdX = canvasWidth / 2;
+  birdY = canvasHeight / 2.3;
   gravity = 0.5;
   birdVelocity = 0;
   birdAcceleration = 0;
@@ -41,40 +45,40 @@ function setup() {
   xPipe2 = width + 300;
   yTopPipe1 = 300;
   yTopPipe2 = 500;
-  spaceBetween = 200;
+  spaceBetween = windowHeight / 2.1;
+  state = 2;
 }
 
+// centering the canvas
 function positionCanvas() {
   canvas.position(windowWidth / 2 - canvasWidth / 2, windowHeight / 2 - canvasHeight / 2);
 }
 
+// executes 60 times per second
 function draw() {
   background(0);
   positionCanvas();
   image(map, 0, 0);
-  replaceBottomGreenBar();
-  firstPipe();
-  secondPipe();
-  grav();
-  image(bird, birdX, birdY, 90.48, 90.24);
+  statesFunction();
 }
 
+// physics
 function grav() {
   imageMode(CENTER);
   birdVelocity += birdAcceleration;
   birdVelocity += gravity;
   birdY += birdVelocity;
   birdAcceleration = 0;
+  // if (birdY <= )
 }
 
-// the bird distance jumped must be 20, no more or less regardless of gravity
-
+// jump when key pressed
 function keyPressed() {
   birdAcceleration = -20;
+  print([mouseX, mouseY]);
 }
 
-// gravity increases with time, when bird jumps gravity resets
-
+// moves the green bar at the bottom
 function replaceBottomGreenBar() {
   image(greenBar1, xBar1, yBar, width);
   image(greenBar2, xBar2, yBar, width);
@@ -92,26 +96,61 @@ function replaceBottomGreenBar() {
   }
 }
 
+// moving the first pair of pipes
 function firstPipe() {
+  // when the pipes get out of the screen, returns to right side of screen with a new random height
   if (xPipe1 > -topPipe.width / 2) {
     xPipe1 -= 2.5;
   } else {
-    yTopPipe1 = random(100, yBar - spaceBetween);
+    yTopPipe1 = random(0, yBar - spaceBetween);
     xPipe1 = width + topPipe.width / 2;
   }
   imageMode(CENTER);
-  image(topPipe, xPipe1, 0, 100, yTopPipe1);
-  image(bottomPipe, xPipe1, yTopPipe1 + spaceBetween, 100, yTopPipe1);
+  image(topPipe, xPipe1, yTopPipe1);
+  image(bottomPipe, xPipe1, yTopPipe1 + spaceBetween);
 }
 
+// moving the second pair of pipes
 function secondPipe() {
+  // when the pipes get out of the screen, returns to right side of screen with a new random height
   if (xPipe2 > -topPipe.width / 2) {
     xPipe2 -= 2.5;
   } else {
-    yTopPipe2 = random(100, yBar - spaceBetween);
+    yTopPipe2 = random(0, yBar - spaceBetween);
     xPipe2 = width + topPipe.width / 2;
   }
   imageMode(CENTER);
-  image(topPipe, xPipe2, 0, 100, yTopPipe2);
-  image(bottomPipe, xPipe2, yTopPipe2 + spaceBetween, 100, yTopPipe2);
+  image(topPipe, xPipe2, yTopPipe2);
+  image(bottomPipe, xPipe2, yTopPipe2 + spaceBetween);
+}
+
+// collision detection for the bird
+function collision() {
+  if (birdY >= yBar || birdY <= 0) {
+    state = 3;
+  }
+  // PLEASE ADD GREEN PIPE COLLISIONS
+}
+
+function statesFunction() {
+  if (state === 1) {
+    imageMode(CENTER);
+    image(playButton, canvasWidth / 2, canvasHeight / 1.6, playButton.width * 0.5, playButton.height * 0.5);
+    image(title, canvasWidth / 2, canvasHeight / 4.25, title.width * 0.124, title.height * 0.124);
+    image(bird, birdX, birdY, bird.width * 0.2, bird.height * 0.2);
+  } else if (state === 2) {
+    replaceBottomGreenBar();
+    firstPipe();
+    secondPipe();
+    grav();
+    image(bird, birdX, birdY, 90.48, 90.24);
+    collision();
+  } else {
+    image(topPipe, xPipe1, yTopPipe1);
+    image(bottomPipe, xPipe1, yTopPipe1 + spaceBetween);
+    image(topPipe, xPipe2, yTopPipe2);
+    image(bottomPipe, xPipe2, yTopPipe2 + spaceBetween);
+    imageMode(CENTER);
+    image(gameOver, canvasWidth / 2, canvasHeight / 4.25);
+  }
 }
