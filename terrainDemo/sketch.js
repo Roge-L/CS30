9fb2b471febac18ecc417d5c617096665cbf367c
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-03-19 15:10:33 -0600

    terrainDemo

--- terrainDemo/sketch.js
+++ terrainDemo/sketch.js
@@ -7,16 +7,33 @@
 // the preload function guarentees that the code inside the function is
 // executed before the rest of the program runs -- helpful for things
 // like loading images
-function preload() {
-}
+let heights = [];
+let numberOfRectangles = 25;
 
 // the setup function will only run once (before the draw loop begins)
 // this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  generateInitialTerrain(numberOfRectangles);
 }
 
 //
 function draw() {
   background(255);
+  displayTerrain();
+}
+
+function generateInitialTerrain(numberOfRectangles) {
+  for (let i = 0; i < numberOfRectangles; i++) {
+    heights.push(random(100, 500));
+  }
+}
+
+function displayTerrain() {
+  let rectWidth = width / numberOfRectangles;
+  rectMode(CORNERS);
+  fill(0);
+  for (let i = 0; i < numberOfRectangles; i++) {
+    rect(i * rectWidth, height, (i + 1) * rectWidth, height - heights[i]);
+  }
 }
