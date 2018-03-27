cb75806e7f00d992f9feb4ab19048d4be9200f62
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-03-27 14:21:00 -0600

    stuff

---
+++ 2dArrays/sketch.js
@@ -0,0 +1,71 @@
+// Title
+// Roger Lam
+// March 5, 2018
+
+// global variables
+
+// the preload function guarentees that the code inside the function is
+// executed before the rest of the program runs -- helpful for things
+// like loading images
+let rows = 100;
+let cols = 100;
+let cellSize;
+let grid;
+
+// the setup function will only run once (before the draw loop begins)
+// this is where you want to set up the environment (size of canvas, etc)
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cellSize = width / cols;
+  grid = createRandom2dArray(cols, rows);
+}
+
+function draw() {
+  background(255);
+  displayGrid();
+}
+
+function displayGrid() {
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      if (grid[x][y] === 0) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function mousePressed() {
+  let xcoord = floor(mouseX / cellSize);
+  let ycoord = floor(mouseY / cellSize);
+
+  if (grid[xcoord][ycoord] === 1) {
+    grid[xcoord][ycoord] = 0;
+  }
+  else {
+    grid[xcoord][ycoord] = 1;
+  }
+}
+
+function keyPressed() {
+  grid = createRandom2dArray(cols, rows);
+}
+
+function createRandom2dArray() {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(0);
+      }
+      else {
+        randomGrid[x].push(1);
+      }
+    }
+  }
+  return randomGrid;
+}
