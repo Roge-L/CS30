b01d4faaa76e7ebf612fc3b4cd55e61ef33085f3
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-04-06 10:26:31 -0600

    basic 2d grid with random bitcoins

--- 2dArrayGame/sketch.js
+++ 2dArrayGame/sketch.js
@@ -4,15 +4,58 @@
 
 // global variables
 
+let grid;
+let cols, rows;
+let cellSize;
+
 // the setup function will only run once (before the draw loop begins)
 // this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cols = 10;
+  rows = 10;
+  grid = gridCreation(cols, rows);
+  cellSize = 60;
 }
 
 // a loop that executes given actions according to your fps
 function draw() {
   background(255);
+  displayGrid();
+}
+
+function gridCreation(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 1) {
+        randomGrid[x].push("bitcoin");
+      }
+      else {
+        randomGrid[x].push(0);
+      }
+    }
+  }
+  return randomGrid;
+}
+
+function spawnItems() {
+  print("not yet");
+}
+
+function displayGrid() {
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      if (grid[x][y] === 0) {
+        fill(85, 64, 16);
+      }
+      else {
+        fill(228, 187, 64);
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
 }
 
 // you are broke.
@@ -21,6 +64,7 @@ function draw() {
 
 // NEEDS:
 // 2d grid
+// random items
 // timer for each square, starts counting down when player uncovers it
 // reward system/inventory system
 
