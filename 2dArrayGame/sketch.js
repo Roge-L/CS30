6fc6871d93e46450379015ac24e4e87350e099dc
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-04-12 15:20:21 -0600

    new need

--- 2dArrayGame/sketch.js
+++ 2dArrayGame/sketch.js
@@ -7,7 +7,7 @@
 let grid;
 let cols, rows;
 let cellSize;
-let bitcoin, portalGun, poop;
+let bitcoin, portalGun, poop, tRex;
 
 // the setup function will only run once (before the draw loop begins)
 // this is where you want to set up the environment (size of canvas, etc)
@@ -15,6 +15,7 @@ function preload() {
   bitcoin = loadImage("assets/images/bitcoin.png");
   portalGun = loadImage("assets/images/portalGun.png");
   poop = loadImage("assets/images/poop.png");
+  tRex = loadImage("assets/images/tRex.png");
 }
 
 function setup() {
@@ -28,7 +29,7 @@ function setup() {
 // a loop that executes given actions according to your fps
 function draw() {
   background(255);
-  displayGrid();
+  displayArtifactsGrid();
 }
 
 function gridCreation(cols, rows) {
@@ -36,12 +37,14 @@ function gridCreation(cols, rows) {
   for (let x = 0; x < cols; x++) {
     randomGrid.push([]);
     for (let y = 0; y < rows; y++) {
-      if (random(20) < 1) {
+      if (random(1500) < 1) {
         randomGrid[x].push("bitcoin");
-      } else if (random(20) < 1) {
+      } else if (random(1500) < 1) {
         randomGrid[x].push("portalGun");
-      } else if (random(20) < 1) {
+      } else if (random(1000) < 1) {
         randomGrid[x].push("poop");
+      } else if (random(10) < 1) {
+        randomGrid[x].push("tRex");
       } else {
         randomGrid[x].push(0);
       }
@@ -50,29 +53,51 @@ function gridCreation(cols, rows) {
   return randomGrid;
 }
 
+function checkDinoSurroundings() {
+  for (let i = 1; i < 4; i++) {
+    for (let j = 1; i < 4; j++) {
+      if (grid[x - i][y - j] != "bitcoin" && grid[x - i][y - j] != "poop" && grid[x - i][y - j] != "portalGun") {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+}
 
-function displayGrid() {
-  fill(85, 64, 16);
+function displayArtifactsGrid() {
+  fill(40, 22, 11);
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
       if (grid[x][y] === 0) {
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        // rect(x * cellSize, y * cellSize, cellSize, cellSize);
       } else if (grid[x][y] === "bitcoin") {
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        // rect(x * cellSize, y * cellSize, cellSize, cellSize);
         image(bitcoin, x * cellSize, y * cellSize, cellSize, cellSize);
       } else if (grid[x][y] === "portalGun") {
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        // rect(x * cellSize, y * cellSize, cellSize, cellSize);
         if (x * cellSize - cellSize >= 0) {
-          if (grid[x - 1][y] >= 0 && grid[x-1][y] != "bitcoin" && grid[x-1][y] != "poop" && grid[x-1][y] != "portalGun") {
+          if (grid[x - 1][y] >= 0 && grid[x - 1][y] != "bitcoin" && grid[x - 1][y] != "poop" && grid[x - 1][y] != "portalGun") {
             image(portalGun, x * cellSize - cellSize, y * cellSize, cellSize * 2, cellSize);
           }
         }
       } else if (grid[x][y] === "poop") {
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        // rect(x * cellSize, y * cellSize, cellSize, cellSize);
         image(poop, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else {
+        // rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        if (x * cellSize - 5 * cellSize >= 0 && y * cellSize - 5 * cellSize >= 0) {
+          image(tRex, x * cellSize - 5 * cellSize, y * cellSize - 5 * cellSize, 5 * cellSize, 5 * cellSize);
+        }
       }
     }
   }
+}
+
+function displayDirtLayer() {
+  fill(85, 64, 16);
 }
 
 // you are broke.
