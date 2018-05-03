f1561949778f7f9502dda6282194937a7e56fd77
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-05-03 14:38:50 -0600

    new template

--- template/sketch.js
+++ template/sketch.js
@@ -1,23 +1,11 @@
-// Title
+// Project Name
 // Roger Lam
-// April 1, 2018
+// Month Day, Year
 
-// global variables
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images
-function preload() {
-
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // put setup code here
 }
 
-// a loop that repeats given actions according to your fps
 function draw() {
-  background(255);
+  // put drawing code here
 }
